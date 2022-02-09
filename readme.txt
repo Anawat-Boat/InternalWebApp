3b2d34c93394f094e784e59ced5ae0c53bfb79f4
anawat.mon.developer@gmail.com <52477826+Anawat-M@users.noreply.github.com>
2022-02-09 11:57:31 +0700

    Layout

---
+++ readme.txt
@@ -0,0 +1,6 @@
+1. create project
+npx create-react-app "project name" 
+2. install material-ui core
+yarn add @material-ui/core
+yarn add @material-ui/icons
+yarn add axios chart.js react-chartjs-2 react-moment react-number-format react-router-dom redux react-redux formik redux-logger redux-thunk url-join clsx @material-ui/lab  jsonwebtoken formik-material-ui
--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "@material-ui/core";
+import Header from "./components/fagments/Header";
+import Menu from "./components/fagments/Menu";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
 
-function App() {
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+
+export default function App() {
+  const handleDrawerClose = () => {
+    setopenDrawer(false);
+  };
+
+  const handleDrawerOpen = () => {
+    setopenDrawer(true);
+  };
+
+  const [openDrawer, setopenDrawer] = React.useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header open={openDrawer} handleDrawerOpen={handleDrawerOpen} />
+      <Menu open={openDrawer} handleDrawerClose={handleDrawerClose} />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Navigate replace to="/login" />} /> //*
+        Redirect V6 */
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
